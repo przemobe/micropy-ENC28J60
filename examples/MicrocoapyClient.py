8a4d41fb22caa3f1767358163289b965b2c3d360
przemobe <przemobe@users.noreply.github.com>
2023-11-21 22:16:09 +0100

    CoAP over ENC28J60

---
+++ examples/MicrocoapyClient.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+#   Copyright 2023 Przemyslaw Bereski https://github.com/przemobe/
+
+#   MicroPython RP2
+#   ENC28J60 (https://github.com/przemobe/micropy-ENC28J60) and
+#   microCoAPy (https://github.com/insighio/microCoAPy) integration.
+
+#   CoAP client and server example.
+
+import Ntw
+from machine import Pin
+from machine import SPI
+
+try:
+    import Dhcp4Client
+except ImportError:
+    Dhcp4Client = None
+
+import microcoapy
+from udpSocket import UdpSocket
+import time
+
+
+def receivedMessageCallback(packet, sender):
+    print(f'[CoAP] Message received: {packet.toString()}, format: {packet.content_format}, from: {sender}')
+    if packet.payload is not None:
+        try:
+            print(f'[CoAP] Message payload: {packet.payload.decode("unicode_escape")}')
+        except Exception as e:
+            print(f'[CoAP] Exception: {e}')
+
+
+def testRequestCallback(packet, senderIp, senderPort):
+    print(f'[CoAP] Test request received: {packet.toString()} from: {senderIp}:{senderPort}')
+    coap.sendResponse(senderIp, senderPort, packet.messageid, 'This is test message response from ENC28J60 RP2.',
+        microcoapy.COAP_RESPONSE_CODE.COAP_CONTENT, microcoapy.COAP_CONTENT_FORMAT.COAP_TEXT_PLAIN, packet.token)
+
+
+def discoveryRequestCallback(packet, senderIp, senderPort):
+    print(f'[CoAP] "well-known/core" received: {packet.toString()} from: {senderIp}:{senderPort}')
+    response = b'</test>;rt="test";ct=0'
+    coap.sendResponse(senderIp, senderPort, packet.messageid, response,
+        microcoapy.COAP_RESPONSE_CODE.COAP_CONTENT, microcoapy.COAP_CONTENT_FORMAT.COAP_NONE, packet.token)
+
+
+if __name__ == "__main__":
+    # Create network
+    nicSpi = SPI(1, baudrate=10000000, sck=Pin(10), mosi=Pin(11), miso=Pin(8))
+    nicCsPin = Pin(13)
+    ntw = Ntw.Ntw(nicSpi, nicCsPin)
+
+    # Setup network IP layer
+    dhcp_client = None
+    if Dhcp4Client is None:
+        # Set static IP address
+        ntw.setIPv4([192,168,40,233], [255,255,255,0], [192,168,40,1])
+    else:
+        # Create DHCP client
+        dhcp_client = Dhcp4Client.Dhcp4Client(ntw)
+
+    # Setup CoAP
+    remoteServerIpStr = "134.102.218.18"
+    remoteServerPort = 5683
+    remoteServerIpBytes = bytes([int(x) for x in remoteServerIpStr.split('.')])
+
+    coap = microcoapy.Coap()
+    coap.responseCallback = receivedMessageCallback
+    coap.addIncomingRequestCallback('test', testRequestCallback)
+    coap.addIncomingRequestCallback('.well-known/core', discoveryRequestCallback)
+
+    coapSocket = UdpSocket(ntw, microcoapy.coap_macros._COAP_DEFAULT_PORT)
+    #coapSocket.debug = True
+    coap.setCustomSocket(coapSocket)
+
+    coapClient_startTime = time.time()
+    coapClient_doneFlag = False
+
+    # Main loop
+    while True:
+        ntw.rxAllPkt()
+        if dhcp_client is not None:
+            dhcp_client.loop()
+
+        coap.loop(False)
+
+        # CoAP client - send one time request(s) to remote server after start
+        ctime = time.time()
+        if (False == coapClient_doneFlag) and (ctime - coapClient_startTime > 1):
+            startTime = ctime
+            if not ntw.isConnectedIp4(remoteServerIpBytes):
+                ntw.connectIp4(remoteServerIpBytes)
+            else:
+                #coap.get(remoteServerIpStr, remoteServerPort, ".well-known/core")
+                coap.get(remoteServerIpStr, remoteServerPort, "test")
+                coapClient_doneFlag = True
---
+++ examples/udpSocket.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+#   Copyright 2023 Przemyslaw Bereski https://github.com/przemobe/
+
+#   MicroPython RP2
+#   ENC28J60 (https://github.com/przemobe/micropy-ENC28J60) and
+#   microCoAPy (https://github.com/insighio/microCoAPy) integration.
+
+#   Custom socket implementation.
+
+class UdpSocket:
+    def __init__(self, ntw, localPort):
+        self.ntw = ntw
+        self.localPort = localPort
+        self.rxQueue = []
+        self.debug = False
+        self.warning = True
+        self.ntw.registerUdp4Callback(self.localPort, self.handleIncomingPacket)
+
+
+    def __del__(self):
+        self.close()
+
+
+    def close(self):
+        self.ntw.registerUdp4Callback(self.localPort, None)
+
+
+    def sendto(self, txBytes, address):
+        if self.debug:
+            print(f'[UdpSocket] Sending {len(txBytes)} bytes to: {str(address)}')
+        addressIp = bytes([int(x) for x in address[0].split('.')])
+
+        if not self.ntw.isConnectedIp4(addressIp):
+            self.ntw.connectIp4(addressIp)
+            if self.warning:
+                print(f'[UdpSocket] Not reachable address: {address[0]}!')
+            return -1
+
+        n = self.ntw.sendUdp4(addressIp, address[1], txBytes, 5683)
+        return n
+
+
+    def recvfrom(self, bufSize):
+        if 0 == len(self.rxQueue):
+            return (None, ('', 0)) # (buffer, remoteAddress), None in case no data
+
+        rxData = self.rxQueue.pop(0)
+        if self.debug:
+            print(f'[UdpSocket] Receiving {len(rxData[0])} bytes to bufffer size {bufSize}')
+
+        if bufSize < len(rxData[0]):
+            rxData[0] = rxData[0][0:bufSize]
+
+        return rxData # (buffer, remoteAddress)
+
+
+    def setblocking(self, flag):
+        # blocking is not supported.
+        pass
+
+
+    def handleIncomingPacket(self, pkt):
+        remoteAddress = (f'{pkt.ip_src_addr[0]}.{pkt.ip_src_addr[1]}.{pkt.ip_src_addr[2]}.{pkt.ip_src_addr[3]}', pkt.udp_srcPort)
+
+        if self.debug:
+            print(f'[UdpSocket] Equeue received {len(pkt.udp_data)} bytes from: {str(remoteAddress)}')
+
+        self.ntw.addArpEntry(pkt.ip_src_addr, pkt.eth_src)
+        self.rxQueue.append((bytes(pkt.udp_data), remoteAddress))
